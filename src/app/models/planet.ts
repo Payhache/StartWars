19c6b1df98b937d0f19838adf2f9671ee095eda9
Pierre Henri <payhache@gmail.com>
2020-06-23 09:59:05 +0200

    changement graphique des planètes

--- src/app/models/planet.ts
+++ src/app/models/planet.ts
@@ -5,13 +5,15 @@ export class Planet {
     allegiance: string;
     nbKmTerre: number;
     img: string;
-    constructor(id: number=null,img:string=null, nom: string = null, nbHabitant: number = null , allegiance: string = null , nbKmTerre: number = null) {
+    description:string;
+    constructor(id: number=null,img:string=null, nom: string = null, nbHabitant: number = null , allegiance: string = null , nbKmTerre: number = null, description:string = null) {
       this.id = id;
       this.img = img;
       this.nom = nom;
       this.nbHabitant = nbHabitant;
       this.allegiance = allegiance;
       this.nbKmTerre = nbKmTerre;
+      this.description = description;
     }
   }
   
